b57edea1636312b1fea854063db12960dc259382
Paulo Barcelos <paulo@azucrina.org>
2020-09-28 09:36:47 +0200

    Added some certbot dependencies

--- .platform/hooks/predeploy/00_setup_ssl_certificates.sh
+++ .platform/hooks/predeploy/00_setup_ssl_certificates.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Certbot dependecies
+sudo wget -r --no-parent -A 'epel-release-*.rpm' http://dl.fedoraproject.org/pub/epel/7/x86_64/Packages/e/
+sudo rpm -Uvh dl.fedoraproject.org/pub/epel/7/x86_64/Packages/e/epel-release-*.rpm
+sudo yum-config-manager --enable epel*
+
 # Download certbot
 wget https://dl.eff.org/certbot-auto
 mv certbot-auto /usr/local/bin/certbot-auto
