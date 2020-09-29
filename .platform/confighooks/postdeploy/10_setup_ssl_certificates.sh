76c3fe1a27955a977b4a169b6d5bcdd470bafce6
Paulo Barcelos <paulo@azucrina.org>
2020-09-29 17:56:52 +0200

    silencing  yum-config-manager

--- .platform/confighooks/postdeploy/10_setup_ssl_certificates.sh
+++ .platform/confighooks/postdeploy/10_setup_ssl_certificates.sh
@@ -5,7 +5,7 @@ echo "LOG: Installing certbot"
 sudo wget -r --no-parent -A 'epel-release-*.rpm' http://dl.fedoraproject.org/pub/epel/7/x86_64/Packages/e/
 sudo rpm -Uvh dl.fedoraproject.org/pub/epel/7/x86_64/Packages/e/epel-release-*.rpm
 sudo yum-config-manager --enable epel*
-sudo yum -y install certbot python2-certbot-nginx
+sudo yum -y install certbot python2-certbot-nginx > /dev/null
 
 # Generate certificates
 echo "LOG: Generating certificates"
--- .platform/hooks/postdeploy/10_setup_ssl_certificates.sh
+++ .platform/hooks/postdeploy/10_setup_ssl_certificates.sh
@@ -4,7 +4,7 @@
 echo "LOG: Installing certbot"
 sudo wget -r --no-parent -A 'epel-release-*.rpm' http://dl.fedoraproject.org/pub/epel/7/x86_64/Packages/e/
 sudo rpm -Uvh dl.fedoraproject.org/pub/epel/7/x86_64/Packages/e/epel-release-*.rpm
-sudo yum-config-manager --enable epel*
+sudo yum-config-manager --enable epel*  > /dev/null
 sudo yum -y install certbot python2-certbot-nginx
 
 # Generate certificates
