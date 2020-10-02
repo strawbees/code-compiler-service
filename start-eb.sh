367bb7b7b21e48b8ae9135f706a7bdfcdd6edc59
Paulo Barcelos <paulo@azucrina.org>
2020-10-02 09:52:26 +0200

    added some logs

--- start-eb.sh
+++ start-eb.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 # Fix the permissions before starting the app
+echo "LOG: Fixing compiler permissions"
 sudo chown -R webapp:webapp /var/app/staging
 
 # Now start
+echo "LOG: Starting app"
 npm run start
