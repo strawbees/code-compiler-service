77361646b853672156f1ab766d7f9f4201ab8f57
Paulo Barcelos <paulo@azucrina.org>
2020-10-02 12:16:46 +0200

    added some logging

--- postinstall.js
+++ postinstall.js
@@ -6,11 +6,14 @@ const init = async () => {
 		// On Elastic Beanstalk, npm install is run as root, causing the files
 		// genreated by the compiler to not be accessible by the web process.
 		if (process.env.AWS_ELASTIC_BEANSTALK) {
+			console.log('LOG: Fixing permissions')
 			/* eslint-disable global-require */
 			const util = require('util')
 			const exec = util.promisify(require('child_process').exec)
 			/* eslint-enable global-require */
-			await exec('chown -R webapp:webapp ./') // change owner to webapp
+			const { stdout, stderr } = await exec('chown -R webapp:webapp ./') // change owner to webapp
+			console.log('LOG: stdout:', stdout)
+			console.error('LOG: stderr:', stderr)
 		}
 	} catch (error) {
 		console.log(error)
