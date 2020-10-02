36930ea3c8e8f8bb662dc9f6a09ecea388d54ab7
Paulo Barcelos <paulo@azucrina.org>
2020-10-02 09:56:49 +0200

    removed sudo and added some debugging info

--- start-eb.sh
+++ start-eb.sh
@@ -2,7 +2,9 @@
 
 # Fix the permissions before starting the app
 echo "LOG: Fixing compiler permissions"
-sudo chown -R webapp:webapp /var/app/staging
+pwd
+ls /var/app/
+chown -R webapp:webapp /var/app/staging
 
 # Now start
 echo "LOG: Starting app"
