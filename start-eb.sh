a5b789ac642e12b1852954f776af0a6e07646dd3
Paulo Barcelos <paulo@azucrina.org>
2020-10-02 10:01:44 +0200

    trying another way to change permission

--- start-eb.sh
+++ start-eb.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
 # Fix the permissions before starting the app
-echo "LOG: Fixing compiler permissions"
-pwd
-ls /var/app/
-chown -R webapp:webapp /var/app/staging
+echo "LOG: Fixing permissions"
+chown -R webapp:webapp ./
 
 # Now start
 echo "LOG: Starting app"
